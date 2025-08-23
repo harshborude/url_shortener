7e072fc8d1b396a8deb8c4749e870ca6f3044063
harshborude <harshborude11@gmail.com>
2025-08-23 10:34:38 +0530

    added login route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,11 @@ import express from 'express';
 import { db } from '../db/index.js'; // <-- Corrected
 import { usersTable } from '../models/index.js'; // <-- Corrected
 // import { createHmac, randomBytes } from 'crypto';
-import { signupPostRequestBodySchema } from '../validation/request.validation.js';
+import { signupPostRequestBodySchema, loginPostRequestBodySchema} from '../validation/request.validation.js';
 // import { eq } from 'drizzle-orm'; // <-- Add this import
-import {hashPasswordWithSalt} from '../utils/hash.js'
+import {hashPasswordWithSalt, } from '../utils/hash.js'
 import {getUserByEmail} from '../services/user.service.js';
-
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 router.post('/signup', async (req, res)=>{
@@ -50,7 +50,28 @@ return res.status(201).json({data : {userId : user[0].id}});
  });
 
  router.post('/login', async(req,res)=>{
+    const validationResult = await loginPostRequestBodySchema.safeParseAsync(req.body)
 
+    if(validationResult.error){
+        return res.status(400).json({error : validationResult.error.format()})
+    }
+    const {email, password} = validationResult.data;
+
+    const user = await getUserByEmail(email);
+
+    if(!user){
+        return res.status(404).json({error:`user with email ${email} does not exists`});
+    }
+
+    const {password : hashedPassword} = hashPasswordWithSalt(password, user.salt);
+
+    if(user.password !== hashedPassword) {
+        return res.status(400).json({error :" Invalid password"});
+    }
+
+    const token = jwt.sign({id:user.id }, process.env.JWT_SECRET);
+
+    return res.json({token});
  })
 
 export default router;
--- validation/request.validation.js
+++ validation/request.validation.js
@@ -6,3 +6,9 @@ export const signupPostRequestBodySchema = z.object({
     email: z.string().email(),
     password: z.string().min(3)
 })
+
+
+export const loginPostRequestBodySchema = z.object({
+    email: z.string().email() ,
+    password : z.string().min(3),
+})
